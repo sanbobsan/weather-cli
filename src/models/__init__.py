b1e67a89f36fed650aad01d6ef4b8c5c69548e05
sanbob <saintsanbob@gmail.com>
2026-02-22 14:51:19 +0800

    feat: pydantic weather models
    - add location, weather, forecast models
    - add models folder
    - ForecastType now in models

---
+++ src/models/__init__.py
@@ -0,0 +1,15 @@
+from .models import (
+    CurrentForecast,
+    DailyForecast,
+    ForecastType,
+    HourlyForecast,
+    Location,
+)
+
+__all__ = [
+    "CurrentForecast",
+    "DailyForecast",
+    "ForecastType",
+    "HourlyForecast",
+    "Location",
+]
